bcc94df5bfb088acc6eb0a527ec69d052a492b7f
Spoon2 <viktor.spoon2@gmail.com>
2022-05-06 15:22:41 +0400

    Program created

---
+++ task/Program.cs
@@ -0,0 +1,31 @@
+﻿/* Написать программу, которая из имеющегося массива строк формирует массив
+из строк, длина которых меньше либо равно 3 символа. Первоначальный массив 
+можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. При 
+решении не рекоммендуется пользоваться коллекциями, лучше обойтись 
+исключительно массивами. */
+
+string[] array = new string[6] { "123", "array", "homework", "GB", ":-)", "Viktor" };
+string[] newArray = new string[array.Length];
+
+FillArray(array, newArray);
+PrintArray(newArray);
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write($"{array[i]}" + " ");
+    }
+}
+void FillArray(string[] array, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            array2[count] = array[i];
+            count++;
+        }
+    }
+}
